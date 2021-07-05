6e1ee70f122bef9170d96b066c8f4ba2dddb6ee6
Yuri Ramos <yuri.ramos@anedot.com>
2021-07-05 16:24:07 -0300

    Update Button and textArea styles

--- src/components/NoContent/index.tsx
+++ src/components/NoContent/index.tsx
@@ -4,6 +4,6 @@ import "./styles.scss";
 export default () => (
   <article className="no-content">
     <h2>¯\_(ツ)_/¯</h2>
-    <p>There's no messages available.</p>
+    <p>There are no messages available.</p>
   </article>
 );
